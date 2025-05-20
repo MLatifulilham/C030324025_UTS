d009c9cf0b6bcf56cf353425f345e47b9b8f06d8
M Latiful Ilham <akunreallml@gmail.com>
2025-05-20 21:10:08 +0800

    Add menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -2,31 +2,74 @@
 #include <conio.h>
 using namespace std;
 
+int data[6];
+int n=6;
 
-void dMenu(){
+void dMenu() {
 system("cls");
-cout<<"Aplikasi Exchange Sort"<<"\n";       
-cout<<"1. Memasukan Data"<<"\n";            
-cout<<"2. Menampilkan Data"<<"\n";            
-cout<<"3. Sorting"<<"\n";           
-cout<<"4. Sepatah Kata"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+cout << "Aplikasi Exchange Sort\n";
+cout << "1. Memasukkan data\n";
+cout << "2. Menampilkan data\n";
+cout << "3. Sorting\n";
+cout << "4. Sepatah kata\n";
+cout << "5. Exit\n";
+cout << "Masukkan angka: ";
 }
 
-void mPertama(string pesan){
+void mPertama(string pesan) {
 system("cls");
-cout<<"hallo saya menu "<<pesan;
+cout << "Hallo, saya menu " << pesan;
 getch();
 }
 
-void sepatahKata(){
-  system("cls");
-  cout <<"Indonesia bisa, Kiata juara\n @poliban";
-  getch();
+void inputData() {
+    system("cls");
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << i + 1 << ": ";
+        cin >> data[i];
+    }
+    cout << "Data berhasil dimasukkan!";
+    getch();
 }
 
+void tampilData() {
+    system("cls");
+    if (n == 0) {
+        cout << "Belum ada data!";
+    } else {
+        cout << "Data: ";
+        for (int i = 0; i < n; i++) {
+            cout << data[i] << " ";
+        }
+    }
+    getch();
+}
+
+void tukar(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void exchange_sort(int data[]) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (data[i] < data[j])  // descending
+              tukar(&data[i], &data[j]);
+        }
+    }
+}
+
+void sorting() {
+    system("cls");
+    if (n == 0) {
+        cout << "Belum ada data untuk diurutkan!";
+    } else {
+        exchange_sort(data);
+        cout << "Data telah diurutkan!";
+    }
+    getch();
+}
 
 int main() {
 char pl;
@@ -38,23 +81,25 @@ do
   {
    case '1':
     /* code */
-    mPertama("pertama");
+    inputData();
     break;
    case '2':
-    mPertama("ke- dua");
+    tampilData();
     /* code */ 
     break;  
    case '3':
-    mPertama("ke- tiga");
+    sorting();
     /* code */
     break;  
    case '4':
-   sepatahKata();
+    mPertama("ke-empat");
     /* code */
     break;  
   case '5':
-    /* code */
-    break;
+    system("cls");
+    cout << "Martun nuhun";
+    getch();
+ break;
   
   default:
     system("cls");
@@ -63,7 +108,6 @@ do
     break;
   }
 
-
 } while (pl!='5');
-  return 0;
+ return 0;
 }
